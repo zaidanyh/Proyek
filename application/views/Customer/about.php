7a676276746effae671b2ac02a7420f2474a97f8
nzzakkya <50857210+nzzakkya@users.noreply.github.com>
2020-04-09 03:23:09 +0700

    aboutnya sudah kk
    yey

--- application/views/Customer/about.php
+++ application/views/Customer/about.php
@@ -1,24 +1,64 @@
-		<div class="site-section bg-light">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-12 section-title text-center mb-5">
-            <h2 class="d-block">Point Care Laundry Shoes</h2>
-            <p> The best solution for your shoes!</p>
-         </div>
-        </div>
-        
-            <div class="post-entry-1">
-              <img src="<?=base_url('assets/images/img_1.jpg')?>" alt="Image" class="img-fluid">              <h2><a href="blog-single.html"></a></h2>
-              <p>Sepatu kamu kotor? Susah dibersihkan? Atau gak punya waktu untuk membersihkan sepatu? Jangan khawatir, gak perlu panik. Sekarang kamu gak perlu repot-repot bersihin atau mencuci sepatu kamu sendiri. Kini telah hadir “Point Care”, penyelamat sepatu kotor dan juga penyelamat waktu kamu. Point Care adalah bisnis yang khusus melayani dalam bidang jasa cuci atau laundry sepatu. Jasa ini adalah bisnis jasa yang melayani khusus untuk mencuci serta membersihkan sepatu kotor dan bau kamu. Dijamin sepatu kamu menjadi bersih. Berbagai jenis dan macam sepatu bisa dibersihkan disini. Dalam jasa ini, kamu hanya perlu datang ke toko untuk memberikan sepatu kotor kamu. Setelah itu oleh pegawai kamu akan diberikan kode, dimana kode itu akan digunakan untuk mengecek apakah sepatu kamu masih dalam proses ataupun sudah selesai pembersihannya. Kode yang diberikan oleh pegawai dapat dicek melalui website “Point Care”. Pegawai juga akan memberitahu kamu berapa lama estimasi sepatu kamu akan selesai. Jadi kamu tidak perlu sering-sering mengecek kode itu didalam website.</p>
-                <div class="post-meta">
-                <span class="d-block"><a href="#">Feb 2020</a></span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+     <div class="site-content-warp">
+	<div class="main-sidebar">
+		<div class="mb-warp text-center">
+			<a href="<?=base_url('Welcome')?>" class="site-logo">
+				<h2>Point Care</h2>
+				<p>Laundry Shoes</p>
+			</a>
+			<div class="about-info">
+        <h2>The best solution for your shoes!</h2>
+        <p>Sepatu kamu kotor? Susah dibersihkan? Atau gak punya waktu untuk membersihkan sepatu? Jangan khawatir, gak perlu panik. Sekarang kamu gak perlu repot-repot bersihin atau mencuci sepatu kamu sendiri. Kini telah hadir “Point Care”, penyelamat sepatu kotor dan juga penyelamat waktu kamu. Point Care adalah bisnis yang khusus melayani dalam bidang jasa cuci atau laundry sepatu. Jasa ini adalah bisnis jasa yang melayani khusus untuk mencuci serta membersihkan sepatu kotor dan bau kamu. Dijamin sepatu kamu menjadi bersih. Berbagai jenis dan macam sepatu bisa dibersihkan disini. Dalam jasa ini, kamu hanya perlu datang ke toko untuk memberikan sepatu kotor kamu. Setelah itu oleh pegawai kamu akan diberikan kode, dimana kode itu akan digunakan untuk mengecek apakah sepatu kamu masih dalam proses ataupun sudah selesai pembersihannya. Kode yang diberikan oleh pegawai dapat dicek melalui website “Point Care”. Pegawai juga akan memberitahu kamu berapa lama estimasi sepatu kamu akan selesai. Jadi kamu tidak perlu sering-sering mengecek kode itu didalam website.</p>
+		
+				</div>
+			<h4 class="pb-3">Contact Us</h4>
+			<div class="text-left ml-3">
+				<a class="text-muted" href="https://goo.gl/maps/ud4wDVf45C8zUkcN9"><i class="fa fa-map-marker"></i> Jl. Kuping Gajah No. 27, Kota Malang</a>
+				<br><a href="https://api.whatsapp.com/send?phone=6285745966039" class="text-muted"><i class="fa fa-phone"></i> +62-813-4602-8188</a>
+				<br><a href="https://gmail.com" class="text-body"><i class="fa fa-envelope"> pointcare@care.id</i></a>
+			</div>
+		</div>
+	</div>
+			<!-- Left Side section end -->
+			<!-- Page start -->
+			<div class="page-section about-page">
+				<div class="about-section">
+          <div class="video-section set-bg" data-setbg="<?=base_url('assets/images/img_1.jpg')?>">
   </div>
-  <div class="footer text-center" height="20" style="border-top: 2px solid #f3f3f3">
-	  <p>Copyright © 2020, All Rights Reserved</p>
-  </div>
+		
+					<div class="row m-0 milestones">
+						<div class="col-lg-3 col-sm-6 p-0">
+							<div class="milestone">
+								<h2>5</h2>
+								<div class="milestone-info">
+									<h5>Years Of experience</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-sm-6 p-0">
+							<div class="milestone">
+								<h2>16k</h2>
+								<div class="milestone-info">
+									<h5>Shoes has been cleaned</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-sm-6 p-0">
+							<div class="milestone">
+								<h2>1k</h2>
+								<div class="milestone-info">
+									<h5>New Customer/Year</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-sm-6 p-0">
+							<div class="milestone">
+								<h2>37k</h2>
+								<div class="milestone-info">
+									<h5>IG Followers</h5>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	
--- application/views/Customer/index.php
+++ application/views/Customer/index.php
@@ -7,8 +7,7 @@
 			</a>
 			<div class="about-info">
 				<h2>The best solution for your shoes!</h2>
-				<p>Sepatu kamu kotor? Susah dibersihkan? Atau gak punya waktu untuk membersihkan sepatu? Jangan khawatir, gak perlu panik. Sekarang kamu gak perlu repot-repot bersihin atau mencuci sepatu kamu sendiri. Kini telah hadir “Point Care”, penyelamat sepatu kotor dan juga penyelamat waktu kamu. Point Care adalah bisnis yang khusus melayani dalam bidang jasa cuci atau laundry sepatu. Jasa ini adalah bisnis jasa yang melayani khusus untuk mencuci serta membersihkan sepatu kotor dan bau kamu. Dijamin sepatu kamu menjadi bersih. Berbagai jenis dan macam sepatu bisa dibersihkan disini. Dalam jasa ini, kamu hanya perlu datang ke toko untuk memberikan sepatu kotor kamu. Setelah itu oleh pegawai kamu akan diberikan kode, dimana kode itu akan digunakan untuk mengecek apakah sepatu kamu masih dalam proses ataupun sudah selesai pembersihannya. Kode yang diberikan oleh pegawai dapat dicek melalui website “Point Care”. Pegawai juga akan memberitahu kamu berapa lama estimasi sepatu kamu akan selesai. Jadi kamu tidak perlu sering-sering mengecek kode itu didalam website.</p>
-			</div>
+				</div>
 			<h4 class="pb-3">Contact Us</h4>
 			<div class="text-left ml-3">
 				<a class="text-muted" href="https://goo.gl/maps/ud4wDVf45C8zUkcN9"><i class="fa fa-map-marker"></i> Jl. Kuping Gajah No. 27, Kota Malang</a>
